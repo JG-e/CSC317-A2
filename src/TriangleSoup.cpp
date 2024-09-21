c7acd55ff7d3889c7cfe324dd9bbdffc174f115a
JG-e <zezhengh@163.com>
2024-09-21 17:58:53 -0400

    Implement first hit intersection in TriangleSoup.cpp

--- src/TriangleSoup.cpp
+++ src/TriangleSoup.cpp
@@ -7,8 +7,7 @@ bool TriangleSoup::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
+  return first_hit(ray, min_t, triangles, t, n);
   ////////////////////////////////////////////////////////////////////////////
 }
 
