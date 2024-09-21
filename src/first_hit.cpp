72cfabc8ef8040a0b4244b30c80e7140f1e8c8f2
JG-e <zezhengh@163.com>
2024-09-21 11:07:04 -0400

    Added first hit implementation

--- src/first_hit.cpp
+++ src/first_hit.cpp
@@ -9,8 +9,20 @@ bool first_hit(
   Eigen::Vector3d & n)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
+  // Loop over all objects in the scene, call their intersect method, and
+  // find the closest intersection.
+  for (int i = 0; i < objects.size(); i++) {
+    double t_obj;
+    Eigen::Vector3d n_obj;
+    if (objects[i]->intersect(ray, min_t, t_obj, n_obj)) {
+      if (t_obj < t) {
+        t = t_obj;
+        n = n_obj;
+        hit_id = i;
+      }
+    }
+  }
+  
   ////////////////////////////////////////////////////////////////////////////
 }
 
