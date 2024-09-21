ccc0569be189dad2a4797a6042acc18a612bbbc3
JG-e <zezhengh@163.com>
2024-09-21 17:55:26 -0400

    Implement triangle-ray intersection in Triangle.cpp

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -2,11 +2,27 @@
 #include "Ray.h"
 #include <Eigen/Dense>
 
+using namespace Eigen;
 bool Triangle::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  Vector3d x1 = corners[0];
+  Vector3d x2 = corners[1];
+  Vector3d x3 = corners[2];
+  Vector3d t1 = x2 - x1; 
+  Vector3d t2 = x3 - x1;
+  
+  Matrix3d A; 
+  A << t1, t2, -ray.direction;
+  Vector3d b = {1, ray.origin - x1, 1};
+  Vector3d x = A.inverse() * b;
+
+  if (x[0] >= 0 && x[1] >= 0 && x[2] >= min_t && x[0] + x[1] <= 1) {
+    t = x[2];
+    n = (x2 - x1).cross(x3 - x1).normalized();
+    return true;
+  }
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }
