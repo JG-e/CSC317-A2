52abd90c117fd7a3bff131978dcb51ac52db393c
JG-e <zezhengh@163.com>
2024-09-21 12:03:43 -0400

    Implement sphere-ray intersection in Sphere.cpp

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,11 +1,37 @@
 #include "Sphere.h"
 #include "Ray.h"
+
+using namespace Eigen;
+using namespace std;
+
 bool Sphere::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
+  Vector3d a = ray.direction.dot(ray.direction);
+  Vector3d b = 2 * ray.direction.dot(ray.origin - center);
+  Vector3d c = (ray.origin - center).dot(ray.origin - center) - radius * radius;
+
+  double discriminant = b * b - 4 * a * c;
+  if (discriminant < 0) {
+    return false;
+  }
+  else {
+    double t1 = (-b + sqrt(discriminant)) / (2 * a);
+    double t2 = (-b - sqrt(discriminant)) / (2 * a);
+    if (t1 < min_t && t2 < min_t) {
+      return false;
+    } else if (t1 < min_t) {
+      t = t2;
+    } else if (t2 < min_t) {
+      t = t1;
+    } else {
+      t = min(t1, t2);
+    }
+    n = (ray.origin + t * ray.direction - center).normalized();
+    return true;
+  }
+
   ////////////////////////////////////////////////////////////////////////////
 }
 
